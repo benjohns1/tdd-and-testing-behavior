59d0ac68aa87f2c82fa547785cc7017ff409afdf
Ben Johns <benjohns.git.gpg.donotreply@gmail.com>
2024-01-24 14:54:34 -0600

    behavior-driven: should save a user to an empty repo

--- behavior-driven/repo/users.go
+++ behavior-driven/repo/users.go
@@ -1,3 +1,18 @@
 package repo
 
-type Users struct{}
+import (
+	app "github.com/benjohns1/tdd-and-testing-behavior/behavior-driven"
+)
+
+type Users struct {
+	user app.User
+}
+
+func (u *Users) Save(user app.User) error {
+	u.user = user
+	return nil
+}
+
+func (u *Users) GetAll() ([]app.User, error) {
+	return []app.User{u.user}, nil
+}
---
+++ behavior-driven/repo/users_test.go
@@ -0,0 +1,45 @@
+package repo_test
+
+import (
+	app "github.com/benjohns1/tdd-and-testing-behavior/behavior-driven"
+	"github.com/benjohns1/tdd-and-testing-behavior/behavior-driven/repo"
+	"reflect"
+	"testing"
+)
+
+func TestUser_Save(t *testing.T) {
+	type args struct {
+		user app.User
+	}
+	tests := []struct {
+		name      string
+		repo      repo.Users
+		args      args
+		wantErr   error
+		wantUsers []app.User
+	}{
+		{
+			name: "should save a user to an empty repo",
+			args: args{
+				user: app.User{Name: "Ender"},
+			},
+			wantUsers: []app.User{{Name: "Ender"}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.repo.Save(tt.args.user)
+			if !reflect.DeepEqual(err, tt.wantErr) {
+				t.Errorf("Save() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			got, err := tt.repo.GetAll()
+			if err != nil {
+				t.Fatal(err)
+			}
+			if !reflect.DeepEqual(got, tt.wantUsers) {
+				t.Errorf("After Save(), GetAll() = %+v, wantUsers %+v", got, tt.wantUsers)
+			}
+		})
+	}
+}
